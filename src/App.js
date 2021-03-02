9d8260d56fe9923734a6823a6cb986dbddd1247b
jiin <dslet2343@gmail.com>
2021-03-02 18:24:43 +0900

    react-ajax 완강

--- src/App.js
+++ src/App.js
@@ -1,113 +1,118 @@
-import React, { useState, useEffect } from "react";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const [funcShow, setFuncShow] = useState(true);
-  const [classShow, setClassShow] = useState(true);
-  return (
-    <div className="container">
-      <h1>Hello World</h1>
-      <input
-        type="button"
-        value="removeFunc"
-        onClick={() => {
-          setFuncShow(false);
-        }}
-      ></input>
-      <input
-        type="button"
-        value="removeClass"
-        onClick={() => {
-          setClassShow(false);
-        }}
-      ></input>
-      {funcShow && <FuncComp initNumber="2"></FuncComp>}
-      {classShow && <ClassComp initNumber="2"></ClassComp>}
-    </div>
-  );
-}
-
-function setRandom() {
-  return Math.floor(Math.random() * 10) + 1;
-}
-
-const funcStyle = "color: skyblue";
-let funcId = 0;
-function FuncComp(props) {
-  const [count, setCount] = useState(props.initNumber);
-  const [_date, setDate] = useState(new Date().toString());
-
-  useEffect(() => {
-    console.log(
-      "%cfunc => effect (componentDidMount & componentDidUpdate)" + ++funcId,
-      funcStyle
+class Nav extends Component {
+  render() {
+    const { list } = this.props;
+    return (
+      <nav>
+        <ul>
+          {list.map((v) => (
+            <li key={v.id}>
+              <a
+                href={v.id}
+                onClick={(e) => {
+                  e.preventDefault();
+                  this.props.onClick(v.id);
+                }}
+              >
+                {v.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
     );
-    document.title = `${count}`;
-    return function () {
-      console.log(
-        "%cfunc => effect (componentWillUnMount)" + ++funcId,
-        funcStyle
-      );
-    };
-  }, [count]);
-  // couont 값이 변경되었을 때만 호출하도록 한다.
-  // 빈 배열을 두 번째 인자에 넣을 경우 componentDidMount 효과만 생긴다. (1회이상 실행되지 않음)
-
-  console.log("%cfunc => render" + ++funcId, funcStyle);
-  return (
-    <div className="container">
-      <h2>function style component</h2>
-      <p>{count}</p>
-      <p>{_date}</p>
-      <input
-        type="button"
-        value="random"
-        onClick={() => {
-          setCount(setRandom());
-        }}
-      />
-      <input
-        type="button"
-        value="date"
-        onClick={() => {
-          setDate(new Date().toString());
-        }}
-      />
-    </div>
-  );
+  }
 }
 
-class ClassComp extends React.Component {
+class Article extends Component {
+  render() {
+    const { title, desc } = this.props;
+    return (
+      <article>
+        <h2>{title}</h2>
+        {desc}
+      </article>
+    );
+  }
+}
+
+class NowLoading extends Component {
+  render() {
+    return <div>Now Loading....</div>;
+  }
+}
+
+class App extends Component {
   state = {
-    number: this.props.initNumber,
-    date: new Date().toString(),
+    list: {
+      items: [],
+      isLoading: false,
+    },
+    article: {
+      item: { title: "Welcome", desc: "Hello, react & Ajax" },
+      isLoading: false,
+    },
   };
 
-  // componentDidMount() {
-  //   console.log("mount");
-  // }
+  componentDidMount() {
+    this.setState({
+      list: { ...this.state.list, isLoading: true },
+    });
+
+    fetch("list.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        this.setState({
+          list: {
+            items: json,
+            isLoading: false,
+          },
+        });
+      });
+  }
+
+  loadList(id) {
+    this.setState({
+      article: { ...this.state.article, isLoading: true },
+    });
+
+    fetch(`${id}.json`)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          article: {
+            item: json,
+            isLoading: false,
+          },
+        });
+      });
+  }
 
   render() {
-    // console.log("render");
+    const { title, desc } = this.state.article.item;
+    const { items, isLoading } = this.state.list;
     return (
-      <div className="container">
-        <h2>class style component</h2>
-        <p>{this.state.number}</p>
-        <p>date : {this.state.date}</p>
-        <input
-          type="button"
-          value="random"
-          onClick={() => {
-            this.setState({ number: setRandom() });
-          }}
-        />
-        <input
-          type="button"
-          value="date"
-          onClick={() => {
-            this.setState({ date: new Date().toString() });
-          }}
-        ></input>
+      <div className="App">
+        <h1>Web</h1>
+        {isLoading ? (
+          <NowLoading />
+        ) : (
+          <Nav
+            list={items}
+            onClick={(id) => {
+              this.loadList(id);
+            }}
+          ></Nav>
+        )}
+        {this.state.article.isLoading ? (
+          <NowLoading />
+        ) : (
+          <Article title={title} desc={desc} />
+        )}
       </div>
     );
   }
