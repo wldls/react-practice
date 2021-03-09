84cc839b61dc34e0cc2c0622e368ea718c61c889
jiin <dslet2343@gmail.com>
2021-03-09 18:09:32 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import React, { Component } from "react";
-import "./App.css";
-import AddNumberRoot from "./components/AddNumberRoot";
-import DisplayNumberRoot from "./components/DisplayNumberRoot";
-import List from "./components/List";
+import React from "react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <h1>Root</h1>
-        <AddNumberRoot />
-        <DisplayNumberRoot />
-        <List></List>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div>test</div>;
+};
 
 export default App;
